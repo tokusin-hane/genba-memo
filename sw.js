b9c394fb1dffe50227697a66d0f583f2803f1892
tokusin-hane <esandm4n1@gmail.com>
2026-04-20 08:39:50 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,4 @@
-const CACHE = 'genba-vocab-v1';
-const FILES = ['/', '/index.html'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
-  );
-});
+const CACHE = 'tokushin-memo-v1';
+const ASSETS = ['/', '/tokushin-memo/', '/tokushin-memo/index.html', '/tokushin-memo/data.js'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
